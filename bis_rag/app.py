aaac9e455eee2542aa3d79f1999c7fa2f5db8880
Rohit <2400080088@kluniversity.in>
2026-09-01 12:17:20 +0530

    Allow Render hosts for backend CORS

--- bis_rag/app.py
+++ bis_rag/app.py
@@ -37,6 +37,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://*.onrender.com",
+        "http://*.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
